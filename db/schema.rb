21ee83ccb61fc7c02c72222d0bf633afa95808d9
Shivam Daryanani <daryananis@gmail.com>
2013-04-29 15:18:19 -0700

    add email index to user schema
    This is done because when authenticating user, i am looking up user by email. Creating an index makes the lookup faster

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130429155545) do
+ActiveRecord::Schema.define(:version => 20130429221609) do
 
   create_table "users", :force => true do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
 
 end
