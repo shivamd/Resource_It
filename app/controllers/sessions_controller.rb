46c13db4c53aa49d66685453247457e39fa62415
Shivam Daryanani <daryananis@gmail.com>
2013-04-29 15:36:02 -0700

    success message upon user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-      sign_in user #needs to be implemented
+      sign_in user
+      flash[:success] = "Welcome To Resource It"
       redirect_to user
 		else
 			flash.now[:error] = "Invalid Email/Password combination"
