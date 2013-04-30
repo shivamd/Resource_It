ab10ad1139a273e9f68b694915afaf2688157b66
Shivam Daryanani <daryananis@gmail.com>
2013-04-30 12:57:36 -0700

    before save generates a new token for user
    The purpose of doing this is to authenticate the user when making requests to the site

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password
 	has_secure_password
-
+  before_save :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }, presence: true
 
 	has_many :snippets, dependent: :destroy
+
+	private
+
+		def create_remember_token
+			self.remember_token = SecureRandom.urlsafe_base64
+		end
 end
