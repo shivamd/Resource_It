fdf16854ad042f450ae4ad23663b68aaa66f11e3
Shivam Daryanani <daryananis@gmail.com>
2013-04-29 17:24:09 -0700

    anonymous javascript function that alerts the highlighted text

---
+++ app/assets/javascripts/compile_javascript.js
@@ -0,0 +1,9 @@
+alert(function() {
+		var text;
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != 'Control') {
+        text = document.selection.createRange().text;
+    }
+    return text
+}())
