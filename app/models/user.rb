e548bfbfb3d580067bfe2bb25fe4096e443a6a30
Shivam Daryanani <daryananis@gmail.com>
2013-04-29 17:16:43 -0700

    user model association with snippet model, user has many snippets

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	validates :name, presence: true, length: { maximum: 50 }
-
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-
 	validates :password, length: { minimum: 6 }, presence: true
+
+	has_many :snippets, dependent: :destroy
 end
