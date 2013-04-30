99cdd4f53c89ddea4412f74f477c0d891b23a5ee
Shivam Daryanani <daryananis@gmail.com>
2013-04-29 17:15:34 -0700

    tests for user model having an association with the snippet model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,5 @@ describe User do
 	it { should validate_presence_of(:password) }
 	it { should ensure_length_of(:password).is_at_least(6) }
 
+  it { should have_many(:snippets).dependent(:destroy) }
 end
