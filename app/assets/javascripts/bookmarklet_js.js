983dd35d0e6d2f631b9980db533e3e1c22c5417d
Shivam Daryanani <daryananis@gmail.com>
2013-04-30 19:49:01 -0700

    fixed url in javascript file

--- app/assets/javascripts/bookmarklet_js.js
+++ app/assets/javascripts/bookmarklet_js.js
@@ -7,7 +7,7 @@
     }
     // .attr('data-bookmarklet-token')
     var token = $('[data-bookmarklet-token]').attr('data-bookmarklet-token');
-    $.get('/snippets?content='+encodeURIComponent(text)+'&token='+encodeURIComponent(token), function(data) {
+    $.get('http://127.0.0.1:3000/snippets?content='+encodeURIComponent(text)+'&token='+encodeURIComponent(token), function(data) {
     	alert("Successfully created snippet")
     })
 
