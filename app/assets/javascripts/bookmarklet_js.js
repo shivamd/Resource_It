ac06de3fbe5d1216782fadcb539dc9193fc76b77
Shivam Daryanani <daryananis@gmail.com>
2013-05-01 15:18:31 -0700

    converted jquery code to javascript in order to reduce dependencies and keep it lightweight
    Quite a few websites do not have jquery, instead on injecting jquery into it, it was more reasonable to write the methods in pure javascript

--- app/assets/javascripts/bookmarklet_js.js
+++ app/assets/javascripts/bookmarklet_js.js
@@ -1,15 +1,43 @@
+function getAllElementsWithAttribute(attribute) {
+  var matchingElements = [];
+  var allElements = document.getElementsByTagName('*');
+  for (var i = 0; i < allElements.length; i++) {
+      if (allElements[i].getAttribute(attribute)) {
+        matchingElements.push(allElements[i]);
+      }
+  }
+  return matchingElements;
+}
+
+function loadXMLDoc(url) {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {
+    xmlhttp=new XMLHttpRequest();
+  } else {
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    // 4 means request in finished and response is readyx
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementsByTagName("body")[0].innerHTML=xmlhttp.responseText;
+    }
+  }
+  xmlhttp.open("GET",url,true);
+  xmlhttp.send();
+}
+
+
 (function() {
-		var text;
+	var text;
     if (window.getSelection) {
         text = window.getSelection().toString();
     } else if (document.selection && document.selection.type != 'Control') {
         text = document.selection.createRange().text;
     }
-    // .attr('data-bookmarklet-token')
-    var token = $('[data-bookmarklet-token]').attr('data-bookmarklet-token');
-    $.get('http://127.0.0.1:3000/snippets?content='+encodeURIComponent(text)+'&token='+encodeURIComponent(token), function(data) {
-    	alert("Successfully created snippet")
-    })
-
+    var token = getAllElementsWithAttribute('data-bookmarklet-token')[0].getAttribute('data-bookmarklet-token')
+    loadXMLDoc('http://127.0.0.1:3000/snippets?content='+encodeURIComponent(text)+'&token='+encodeURIComponent(token))
 })()
+
+
+
 
