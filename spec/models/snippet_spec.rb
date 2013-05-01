72b8f719225b3d6d289705f1242aa5272380ffee
Shivam Daryanani <daryananis@gmail.com>
2013-05-01 10:25:55 -0700

    Test for Snippet model association with tag model

--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -5,4 +5,6 @@ describe Snippet do
 	it { should validate_presence_of(:content) }
 	it { should validate_presence_of(:user_id) }
 	it { should belong_to(:user) }
+
+	it { should have_and_belong_to_many(:tags) }
 end
