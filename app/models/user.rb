1dbd9f490ff1180be40ec60129defd1828775658
Shivam Daryanani <daryananis@gmail.com>
2013-05-01 19:56:26 -0700

    removed user name validations
    The user no longer needs to signup with his/her name, they can edit their name later from the profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password
 	has_secure_password
   before_save :create_remember_token
-	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,6 @@ require "spec_helper"
 
 describe User do
 
-	it { should validate_presence_of(:name) }
-	it { should ensure_length_of(:name).is_at_most(50) }
-
 	it { should validate_presence_of(:email) }
 	it { should allow_value("testing@example.com").for(:email) }
 	it { should_not allow_value("test[at]dotcom").for(:email) }
