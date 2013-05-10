0e6b700869ac8186fbd8ec2f1546a3b167fc04a1
Shivam Daryanani <daryananis@gmail.com>
2013-05-10 07:23:33 -0700

    changed url pointer to herokuapp

--- app/helpers/bookmarklet_helper.rb
+++ app/helpers/bookmarklet_helper.rb
@@ -4,7 +4,7 @@ module BookmarkletHelper
 		js = <<-JS
 		(function (){
 			var script = document.createElement('SCRIPT');
-			script.src = 'http://127.0.0.1:3000/assets/bookmarklet_js.js';
+			script.src = 'https://snipp.et.herokuapp.com/assets/bookmarklet_js.js';
 			script.setAttribute('data-bookmarklet-token', '#{current_user.remember_token}');
 			document.body.appendChild(script);
 		})();
