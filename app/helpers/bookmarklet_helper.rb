07e980065e721f22f5f9d8d8d86058621e333acb
Shivam Daryanani <daryananis@gmail.com>
2013-05-10 07:33:38 -0700

    fixed url logic

--- app/helpers/bookmarklet_helper.rb
+++ app/helpers/bookmarklet_helper.rb
@@ -1,11 +1,11 @@
 module BookmarkletHelper
 
 	def bookmarklet_caller_js
-	  base_url = Rails.env.dev? ? "http://#{request.host_and_port}/" ? "https://#{request.host_and_port}/"
+	  base_url = Rails.env.dev? ? "http://#{request.host_and_port}/" : "https://#{request.host_and_port}/"
 		js = <<-JS
 		(function (){
 			var script = document.createElement('SCRIPT');
-			script.src = '#{base_url}/assets/bookmarklet_js.js';
+			script.src = '#{base_url}assets/bookmarklet_js.js';
 			script.setAttribute('data-bookmarklet-token', '#{current_user.remember_token}');
 			document.body.appendChild(script);
 		})();
