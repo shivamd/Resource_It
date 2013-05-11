5128fa4aa8232247a20cd093f9e0afc6dbadbefe
Shivam Daryanani <daryananis@gmail.com>
2013-05-11 10:56:30 -0700

    changed bookmarklet javascript post route

--- app/assets/javascripts/bookmarklet_js.js
+++ app/assets/javascripts/bookmarklet_js.js
@@ -38,7 +38,7 @@ function loadXMLDoc(url) {
     }
     var tags = encodeURIComponent(prompt("Any categories for this snippet? e.g: funny inspirational code design", "snippet"));
     var token = getAllElementsWithAttribute('data-bookmarklet-token')[0].getAttribute('data-bookmarklet-token')
-    loadXMLDoc('http://'+location.host+'/snippets?content='+encodeURIComponent(text)+'&token='+encodeURIComponent(token)+'&tags='+tags)
+    loadXMLDoc('http://snipp-et.herokuapp.com/snippets?content='+encodeURIComponent(text)+'&token='+encodeURIComponent(token)+'&tags='+tags)
 })()
 
 
