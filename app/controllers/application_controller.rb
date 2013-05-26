662222c07ed92006297b875dba6c6e96fc431dbb
Shivam Daryanani <daryananis@gmail.com>
2013-05-27 03:30:32 +0400

    added headers for cross domain ajax call.
    This enables cross-origin resource sharing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://snipp-et.herokuapp.com'' 
+    headers['Access-Control-Request-Method'] = '*' 
+  end
 end
