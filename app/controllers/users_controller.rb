10985fc8412d88ff15e82e9e9e214f31f50708fe
Shivam Daryanani <daryananis@gmail.com>
2013-05-31 05:18:12 +0400

    Home page ajax logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
   respond_to :js, :html
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(params[:user])
-		unless @user.save
-			render :new
-		else
-			sign_in(@user)
-		end
-	end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in(@user)
+    end
+  end
 
-	def show
-		@user = User.includes(:tags).find(params[:id])
-	end
+  def show
+    @user = User.includes(:tags).find(params[:id])
+  end
 end
