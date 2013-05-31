f17ab81f20f39fea7fe9a559370f021d9abd2b44
Shivam Daryanani <daryananis@gmail.com>
2013-05-31 09:11:34 +0400

    changed position of flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      flash[:success] = "Welcome To Resource It"
+      flash[:success] = "Successfully logged in"
       redirect_to user
     else
       flash.now[:error] = "Invalid Email/Password combination"
