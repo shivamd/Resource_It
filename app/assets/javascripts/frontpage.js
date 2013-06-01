3470158691aea376b4fd20f7dad0c8b9e06f9bf5
Shivam Daryanani <daryananis@gmail.com>
2013-06-01 13:00:44 +0400

    Added snippet button on home page, along with hover handler which changes the text

--- app/assets/javascripts/frontpage.js
+++ app/assets/javascripts/frontpage.js
@@ -1,9 +1,14 @@
-
 $(document).ready(function(){
   $('small').click(function(e){
-  	e.preventDefault();
-  	$(this).parent().hide();
-  	$(this).parent().siblings('div').show();
-  	})
+    e.preventDefault();
+    $(this).parent().hide();
+    $(this).parent().siblings('div').show();
+  })
+  $('body').on('mouseenter','a.bookmarklet', function(){
+    $(this).text("SNIPP-ET"); 
+  })
+  $('body').on('mouseleave', 'a.bookmarklet', function(){
+    setTimeout(function(){ $('a.bookmarklet').text("DRAG ME") }, 1000);
+  })
 });
 
